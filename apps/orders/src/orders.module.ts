50828357667e91dfa96b832b9859c1c254feb702
fiknaufalh <18221096@mahasiswa.itb.ac.id>
2024-07-23 11:15:42 +0700

    feat: setup orders and docker mongo

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { Order, OrderSchema } from './schemas/order.schema';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '@app/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+    DatabaseModule,
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
