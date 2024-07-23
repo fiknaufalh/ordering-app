8c3b1f59242a43aaafff5621e541c67583486e69
fiknaufalh <18221096@mahasiswa.itb.ac.id>
2024-07-23 11:42:06 +0700

    feat: implement post order

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -13,6 +13,7 @@ import * as Joi from 'joi';
       isGlobal: true,
       validationSchema: Joi.object({
         MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
       }),
       envFilePath: './apps/orders/.env',
     }),
